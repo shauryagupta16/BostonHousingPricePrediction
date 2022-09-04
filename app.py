015841276a2b84504b3f152d79b749ec8ee4efc7
Shaurya Gupta <shauryagupta207@gmail.com>
2022-09-04 16:12:52 +0530

    these are the updated app and html files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # from copyreg import pickle
 
 
+# from crypt import methods
 import pickle
 from flask import Flask,render_template,request,app,url_for,jsonify
 import numpy as np
@@ -26,6 +27,13 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
